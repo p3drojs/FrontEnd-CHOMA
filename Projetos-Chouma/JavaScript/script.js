8935a84212a9fa7367e83781edbc54f7900f6400
p3drojs <tr4quinas@gmail.com>
2025-03-20 22:20:28 -0300

    comitado

---
+++ Projetos-Chouma/JavaScript/script.js
@@ -0,0 +1,23 @@
+/*var primeiroTipo;
+let segundoTipo;
+const terceiroTipo = 10;
+
+var nome = "joao";
+var idade = "33";
+
+console.log(nome);
+console.log(idade);
+
+nome = idade;
+console.log(nome);
+*/
+
+function escrever(){
+    document.getElementById('texto').innerText = "Oi, vc clicou no meu botão";
+}
+function sally(){
+    document.getElementById('sally').src="imagens/sally.jpg";
+}
+function esconder(){
+    document.getElementById('sally').src="";
+}
