2b74c02b14c69c1386ff8fdee60a138b1cb03ea9
p3drojs <tr4quinas@gmail.com>
2025-03-21 20:18:06 -0300

    comite

---
+++ Projetos-Chouma/javascript2/script.js
@@ -0,0 +1,8 @@
+function ligarLampada(){
+    document.getElementById("lampada");
+    lampada.src = "imagens/lampadaAcessa.jpg";
+}
+
+function desligarLampada(){
+    document.getElementById('lampada').src="imagens/lampadaApagada.jpg"
+}
