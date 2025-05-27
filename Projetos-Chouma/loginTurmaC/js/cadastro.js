7b77e863d15aed6a4776f8793b4bf19f1d96d256
p3drojs <pedroantoni630@gmail.com>
2025-05-26 22:26:01 -0300

    comit

--- Projetos-Chouma/loginTurmaC/js/cadastro.js
+++ Projetos-Chouma/loginTurmaC/js/cadastro.js
@@ -9,5 +9,26 @@ botao.addEventListener('click', function(){
     listaUsuarioCadastrados.push(objusuario);
     const listaJson = JSON.stringify(listaUsuarioCadastrados);
     localStorage.setItem('usuarios', listaJson);
+    listar();
 }
 );
+
+function listar(){
+    const listaUsuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+    let tabela = document.getElementById('listaUsuariosCadastrados');
+    tabela.innerHTML = "";
+    listaUsuarios.forEach((objeto, index) => {
+        let linha = document.createElement('tr');
+        linha.innerHTML = `
+            <td>${objeto.usuario}</td>
+            <td>${objeto.senha}</td>
+            <td>
+                <button>Editar</button>
+                <button>Remover</button>
+            </td>
+        `;
+        tabela.appendChild(linha);
+    });
+}
+
+listar();
